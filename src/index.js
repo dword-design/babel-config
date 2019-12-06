5ea5f21a1ec3cc4d292fe54f195d81ea3912714f
dword <sebastianlandw@gmail.com>
2019-12-06 13:09:28 +0000

    fix alias interface

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import aliases from '@dword-design/aliases'
+import { getStandard as getAliases } from '@dword-design/aliases'
 
 export default {
   presets: [
@@ -9,7 +9,7 @@ export default {
     require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'),
     [require.resolve('@babel/plugin-proposal-pipeline-operator'), { proposal: 'fsharp' }],
     require.resolve('babel-plugin-add-module-exports'),
-    [require.resolve('babel-plugin-module-resolver'), { alias: aliases }],
+    [require.resolve('babel-plugin-module-resolver'), { alias: getAliases() }],
     [require.resolve('babel-plugin-transform-imports'), {
       '@dword-design/functions': { transform: '@dword-design/functions/dist/${member}' },
     }],
