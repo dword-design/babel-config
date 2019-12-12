668f4bedbde00ff19da7d3547a852763cfc1aa91
Sebastian Landwehr <info@dword-design.de>
2019-12-12 12:20:38 +0100

    upgrade packages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import { getStandard as getAliases } from '@dword-design/aliases'
+import getPackageName from 'get-package-name'
 
 export default {
   presets: [
-    [require.resolve('@babel/preset-env'), { targets: { node: 10 } }],
+    [getPackageName(require.resolve('@babel/preset-env')), { targets: { node: 10 } }],
   ],
   plugins: [
-    require.resolve('@babel/plugin-proposal-optional-chaining'),
-    require.resolve('@babel/plugin-proposal-nullish-coalescing-operator'),
-    [require.resolve('@babel/plugin-proposal-pipeline-operator'), { proposal: 'fsharp' }],
-    require.resolve('babel-plugin-add-module-exports'),
-    [require.resolve('babel-plugin-module-resolver'), { alias: getAliases() }],
-    [require.resolve('babel-plugin-transform-imports'), {
-      '@dword-design/functions': { transform: '@dword-design/functions/dist/${member}' },
+    getPackageName(require.resolve('@babel/plugin-proposal-optional-chaining')),
+    getPackageName(require.resolve('@babel/plugin-proposal-nullish-coalescing-operator')),
+    [getPackageName(require.resolve('@babel/plugin-proposal-pipeline-operator')), { proposal: 'fsharp' }],
+    getPackageName(require.resolve('babel-plugin-add-module-exports')),
+    [getPackageName(require.resolve('babel-plugin-transform-imports')), {
+      [getPackageName(require.resolve('@dword-design/functions'))]: { transform: '@dword-design/functions/dist/${member}' },
     }],
   ],
 }
