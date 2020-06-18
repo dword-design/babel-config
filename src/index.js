d7088adcf500aa249ab56a32d03ead7380f836fc
foo <foo@bar.de>
2020-06-18 12:19:47 +0200

    feat: add root feature

--- src/index.js
+++ src/index.js
@@ -1,24 +1,53 @@
+import { startsWith } from '@dword-design/functions'
+import { resolvePath } from 'babel-plugin-module-resolver'
+import findUp from 'find-up'
 import getPackageName from 'get-package-name'
 import { paramCase } from 'param-case'
+import P from 'path'
 
 export default {
   presets: [
-    [getPackageName(require.resolve('@babel/preset-env')), { targets: { node: 10 } }],
+    [
+      getPackageName(require.resolve('@babel/preset-env')),
+      { targets: { node: 10 } },
+    ],
   ],
   plugins: [
     getPackageName(require.resolve('@babel/plugin-proposal-optional-chaining')),
-    [getPackageName(require.resolve('@babel/plugin-proposal-pipeline-operator')), { proposal: 'fsharp' }],
+    [
+      getPackageName(
+        require.resolve('@babel/plugin-proposal-pipeline-operator')
+      ),
+      { proposal: 'fsharp' },
+    ],
     getPackageName(require.resolve('babel-plugin-add-module-exports')),
-    [getPackageName(require.resolve('babel-plugin-module-resolver')), {
-      'alias': {
-        '@': '.',
+    [
+      getPackageName(require.resolve('babel-plugin-module-resolver')),
+      {
+        alias: {
+          '@': '.',
+        },
+        resolvePath: (sourcePath, currentFile, options) => {
+          const rootPath = findUp.sync('.root', { cwd: P.dirname(currentFile) })
+          const rootDir = rootPath ? P.dirname(rootPath) : rootPath
+          return resolvePath(sourcePath, currentFile, {
+            ...options,
+            cwd:
+              rootDir && (rootDir |> startsWith(process.cwd()))
+                ? rootDir
+                : process.cwd(),
+          })
+        },
       },
-      cwd: 'packagejson',
-    }],
-    [getPackageName(require.resolve('babel-plugin-transform-imports')), {
-      [getPackageName(require.resolve('@dword-design/functions'))]: {
-        transform: importName => `@dword-design/functions/dist/${importName |> paramCase}`,
+    ],
+    [
+      getPackageName(require.resolve('babel-plugin-transform-imports')),
+      {
+        [getPackageName(require.resolve('@dword-design/functions'))]: {
+          transform: importName =>
+            `@dword-design/functions/dist/${importName |> paramCase}`,
+        },
       },
-    }],
+    ],
   ],
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,17 +1,23 @@
-import withLocalTmpDir from 'with-local-tmp-dir'
+import { endent, mapValues } from '@dword-design/functions'
 import execa from 'execa'
 import outputFiles from 'output-files'
 import { resolve } from 'path'
-import { endent, mapValues } from '@dword-design/functions'
+import withLocalTmpDir from 'with-local-tmp-dir'
 
-const runTest = config => () => withLocalTmpDir(async () => {
-  await outputFiles({
-    'package.json': JSON.stringify({}),
-    ...config.files,
+const runTest = config => () =>
+  withLocalTmpDir(async () => {
+    await outputFiles({
+      'package.json': JSON.stringify({}),
+      ...config.files,
+    })
+    await execa(
+      'babel',
+      ['--out-dir', 'dist', '--config-file', require.resolve('.'), 'src'],
+      { cwd: config.cwd }
+    )
+    await config.test()
   })
-  await execa('babel', ['--out-dir', 'dist', '--config-file', require.resolve('.'), 'src'])
-  await config.test()
-})
+
 export default {
   'alias: valid': {
     files: {
@@ -19,26 +25,41 @@ export default {
         'bar/index.js': 'export default 1',
         'foo/index.js': endent`
           import bar from '@/src/bar'
-
+          
           export default bar
         `,
       },
     },
     test: () => expect(require(resolve('dist', 'foo'))).toEqual(1),
   },
-  'alias: multiple roots': {
+  'alias: root': {
     files: {
       src: {
         'foo.js': 'export default 1',
-        'package.json': JSON.stringify({}),
+        '.root': '',
         'index.js': endent`
-          import value from '@/foo'
-
-          export default value
+          import foo from '@/foo'
+          
+          export default foo
         `,
       },
     },
     test: () => expect(require(resolve('dist'))).toEqual(1),
+  },
+  'alias: root behind cwd': {
+    files: {
+      '.root': '',
+      'sub/src': {
+        'foo.js': 'export default 1',
+        'index.js': endent`
+          import foo from '@/src/foo'
+          
+          export default foo
+        `,
+      },
+    },
+    cwd: 'sub',
+    test: () => expect(require(resolve('sub', 'dist'))).toEqual(1),
   },
   functions: {
     files: {
@@ -64,5 +85,4 @@ export default {
     },
     test: () => expect(require(resolve('dist'))).toEqual(2),
   },
-}
-  |> mapValues(runTest)
+} |> mapValues(runTest)
