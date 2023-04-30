44fe927284dd6698bf892977c8719842d0573365
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-04-30 04:44:36 +0200

    fix: fix compatible node version (#154)

--- src/create/index.js
+++ src/create/index.js
@@ -54,9 +54,10 @@ export default () => {
     presets: [
       [
         packageName`@babel/preset-env`,
-        packageConfig.type === 'module'
-          ? { modules: false, targets: { node: 14 } }
-          : { targets: { node: 10 } },
+        {
+          ...(packageConfig.type === 'module' ? { modules: false } : {}),
+          targets: { node: 14 },
+        },
       ],
       packageName`@vue/babel-preset-jsx`,
       packageName`@babel/preset-typescript`,
